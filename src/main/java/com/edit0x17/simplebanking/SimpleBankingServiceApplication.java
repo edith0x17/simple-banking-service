2478153869a9c0e73e050fe292f88d69adc5d6ac
edith0x17 <sido117929@gmail.com>
2026-07-16 17:21:36 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/edit0x17/simplebanking/SimpleBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edit0x17.simplebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edit0x17/simplebanking/SimpleBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edit0x17.simplebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
